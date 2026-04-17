bffc0cdf3ae3b9af85125dba662f4f7e79d835cd
emon ahmed <emon9996a@gmail.com>
2026-04-17 21:34:13 +0600

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="py-3 shadow-2xl ">
-      <div className="max-w-7xl items-center justify-between">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
         {/* logo section  */}
         <div className="">
           <Link to={"/"}>
             <h2 className="font-bold text-3xl">
-              {" "}
-              <span className=" text-red-600">Kena</span>Karta{" "}
+              <span className=" text-red-600">Kena</span>Karta
             </h2>
           </Link>
+          <div className="flex gap-1 cursor-pointer text-gray-700 items-center"></div>
         </div>
       </div>
     </div>
