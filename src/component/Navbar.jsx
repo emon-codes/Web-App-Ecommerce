f65f8d2292a1266b2d80352d2997b856351434fb
emon ahmed <emon9996a@gmail.com>
2026-04-19 11:10:45 +0600

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,10 @@
+import { MapPin } from "lucide-react";
 import React from "react";
+import { FaCaretDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const location = false;
   return (
     <div className="py-3 shadow-2xl ">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
@@ -12,7 +15,13 @@ const Navbar = () => {
               <span className=" text-red-600">Kena</span>Karta
             </h2>
           </Link>
-          <div className="flex gap-1 cursor-pointer text-gray-700 items-center"></div>
+          <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
+            <MapPin className="text-red-500" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add Address"}
+            </span>
+            <FaCaretDown />
+          </div>
         </div>
       </div>
     </div>
