e79d9b313a4ad7776f4cd8d7be23a55a3a322b2e
emon ahmed <emon9996a@gmail.com>
2026-04-20 09:02:35 +0600

    Nav link add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import { MapPin } from "lucide-react";
 import React from "react";
 import { FaCaretDown } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const location = false;
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="py-3 shadow-2xl ">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
         {/* logo section  */}
-        <div className="">
+        <div className="flex gap-7 items-center">
           <Link to={"/"}>
             <h2 className="font-bold text-3xl">
               <span className=" text-red-600">Kena</span>Karta
@@ -23,6 +23,43 @@ const Navbar = () => {
             <FaCaretDown />
           </div>
         </div>
+        {/* menu sectioon */}
+        <nav>
+          <ul className="flex gap-7 items-center text-xl font-semibold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 boder-red-800 transition-all" : "text-black"} `
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 boder-red-800 transition-all" : "text-black"} `
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 boder-red-800 transition-all" : "text-black"} `
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"/contacts"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 boder-red-800 transition-all" : "text-black"} `
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+        </nav>
       </div>
     </div>
   );
